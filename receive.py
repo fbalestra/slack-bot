3ba799f1f08f2887ef46150cbd63943418868f70
Julien <julien.crevits@gmail.com>
2016-07-16 00:13:29 +0900

    holy cow Slack buttons starting to work

--- receive.py
+++ receive.py
@@ -1,44 +1,76 @@
 from __future__ import print_function
-
-import os
-import re
-import json
-
+import os, re, slack
+from queryjob import check_job, build_reply
 from flask import Flask, request, Response
-import slack
 
 app = Flask(__name__)
 
-SLACK_SLASH_TOKEN = os.environ.get('SLACK_SLASH_TOKEN')
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
 
-@app.route('/', methods=['POST', 'GET'])
+
+@app.route('/auth', methods=['POST', 'GET'])
+def auth():
+
+    if request.args.get('code'):
+        code = request.args.get('code')
+    else:
+        return Response('No code.'), 401
+
+    print("code: "+code)
+
+    response = slack.get_slack_token(code)
+
+    print("slack response: {0}".format(response))
+
+    return Response(), 200
+
+
+@app.route('/slack', methods=['POST', 'GET'])
 def inbound():
-    if request.form.get('token') != SLACK_SLASH_TOKEN:
+    if request.form.get('token') != SLACK_TOKEN:
         return Response(), 403
 
-    #channel_id = request.form.get('channel_id')
     username = request.form.get('user_name')
     user_id = request.form.get('user_id')
+    channel_id = slack.get_private_channel(user_id)
     text = str(request.form.get('text'))
 
-    # previous version with bot reacting to keyword
-    # m = re.match(r'track (\d+)', text)
     m = re.match(r'(\d+)', text)
 
     if m:
         job_id = int(m.group(1))
 
-        channel_id = slack.get_private_channel(user_id)
-        print (slack.send_message(channel_id,
-                "wait a moment, %s. I'm checking *%d" % (username, job_id)))
+        print (slack.send_message(channel_id, "_Please wait a moment, %s. I am checking *%d_" % (username, job_id)))
 
-        message = "Received message: " + str(job_id)
-        slack.send_message(channel_id, message)
+        # TODO (julien) find a way to reply to Slack immediately and continue after
+        # Response("Got it!"), 200
+
+        job = check_job(job_id)
+        message, attachments = build_reply(job)
+        slack.send_message(channel_id, message, attachments)
+        # TODO (julien) work on daemon + threads
+        # checker.add(job)
     else:
-        slack.send_message(channel_id, "I don't know what to do :(")
-        return Response('unrecognized command'), 400
+        slack.send_message(channel_id, "It looks like you did not enter a valid id")
+        return Response('unrecognized parameter'), 400
     return Response(), 200
-    
+
+
+@app.route('/cweekly', methods=['POST', 'GET'])
+def cweekly():
+
+    print('Cataweekly: {0}'.format(request.form.get('stale_lps')))
+
+    slack.notify_pending_cweekly(request.form.get('stale_lps'))
+
+    return Response('Cweekly request received.'), 200
+
+
+@app.route('/button', methods=['POST', 'GET'])
+def button():
+    print ('Button pressed :D')
+
+    return Response('Button pressed!!!'), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
--- slack.py
+++ slack.py
@@ -1,10 +1,34 @@
 import os
+import requests
+import json
 from slackclient import SlackClient
 
-# TODO(julien) Ask DevOps to configure this environment variable in live environment.
+# TODO(julien) Ask DOps to configure this environment variable in live environment.
 SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 
 slack_client = SlackClient(SLACK_BOT_TOKEN)
+
+
+def get_slack_token(code):
+    if code:
+        payload = {
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'code': code
+        }
+
+        response = requests.post('https://slack.com/api/oauth.access', data=payload)
+
+        result = response.json()
+
+        # if result['error'] == 'invalid_client_id':
+        #    print('client_id: '+CLIENT_ID)
+
+        return result
+
+    return "Something wrong with get_slack_token"
 
 
 def list_channels():
@@ -21,7 +45,7 @@ def send_message(channel_id, message, attachments=None):
         text=message,
         username='julien test',
         attachments=attachments,
-        icon_emoji=':flag-jp:'
+        icon_emoji=':sleuth_or_spy:'
     )
 
 
@@ -31,6 +55,45 @@ def get_private_channel(user_id):
         return private_channel['channel']['id']
     else:
         return None
+
+
+def notify_pending_cweekly(stale_lps):
+
+    buttons = [
+        {
+            "name": 'notifysts',
+            "text": 'notify STs',
+            "type": "button",
+            "value": "ende"
+         }
+    ]
+
+    # buttons = []
+
+    # for k, v in stale_lps.items():
+    #     button = {
+    #         "name": k,
+    #         "text": k+': '+v,
+    #         "type": "button",
+    #         "value": "ende"
+    #     }
+    #
+    #     buttons.append(button)
+
+    attachments = [
+            {
+                "text": "",
+                "fallback": "Unable to notify STs",
+                "callback_id": "cweekly",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": buttons
+            }
+        ]
+
+    attachments = json.dumps(attachments)
+
+    print(send_message("#general", stale_lps, attachments))
 
 
 def main():
